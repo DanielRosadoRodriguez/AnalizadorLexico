89c5fa640c290bdd8f7a11035ceeb3b0a495c620
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-09 13:06:03 -0600

    feat: add module that prints the values of the 2nd file

---
+++ print_2ndo_archivo.py
@@ -0,0 +1,7 @@
+def print_2ndo_archivo(elementos):
+    """
+    funcion que recibe los elementos a imprimir en forma de diccionario
+    Imprime los elementos en un nuevo archivo de texto
+    """
+
+    
