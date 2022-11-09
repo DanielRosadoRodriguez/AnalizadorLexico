3f1feae64938858a90245254e0e6326b4586bd24
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-08 21:16:50 -0600

    feat: add function that prints a message in case of error

---
+++ print_error.py
@@ -0,0 +1,23 @@
+def print_error_message(line, line_number, error_index, hint):
+
+    error_line_message = f""
+    line_str = f""
+
+    for index, element in enumerate(line):
+        line_str += f"{element} "
+        if index == error_index:
+            error_line_message += len(element) * "*"
+        else:
+            error_line_message += len(element) * " "
+        error_line_message += " "
+
+    print(f"- Danger! Error en la linea {line_number}:")
+    if line_number < 2:
+        print(f"            |")
+    else:
+        print(f"          {line_number - 1} | ")
+    print(f"          {line_number} | {line_str}")
+    print(f"          {line_number+1} | {error_line_message}")
+
+    if hint is not None:
+        print(f"              {hint}")
