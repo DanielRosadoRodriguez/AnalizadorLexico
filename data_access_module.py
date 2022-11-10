454b487bb1d3f20d8d974cfe3f59020f05ec245d
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-10 14:47:02 -0600

    feat: add optional path parameter so the user can insert the path to any file

--- data_access_module.py
+++ data_access_module.py
@@ -1,32 +1,15 @@
-def read_text():
-    """
-    funcion que lee el archivo de texto de entrada y
-    regresa su contenido en una varialbe
-    """
-    # inicializamos una lista que contendra los elementos del archivo de texto
+def read_text(path="input.txt"):
+
     tokens = []
-    # leemos el documento con el input
-    with open("input.txt", "r") as file:
-        # recorremos cada linea del archivo de texto
+    with open(path, "r") as file:
         for line in file:
-            # separamos cada linea en sus elementos y las agregamos a la lista de tokens
             tokens.append(line.split())
-    # una vez terminado de guardar los elementos del archivo, cerramos el archivo de texto
     file.close()
     return tokens
 
 
-def print_1er_archivo(elements):
-    """function that writes to a file"""
-    with open("prueba.lex", "a") as out:
+def print_archivo(elements, path="input.txt"):
+    with open(path, "a") as out:
         for element in elements:
             out.write(f"{element}\n")
     out.close()
-
-
-def print_2ndo_archivo(elementos):
-    """
-    funcion que recibe los elementos a imprimir en forma de diccionario
-    Imprime los elementos en un nuevo archivo de texto
-    """
-
