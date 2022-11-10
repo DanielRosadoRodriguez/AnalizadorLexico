daf4c099699bf8d75cd3df237f7016440712d977
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-09 20:39:54 -0600

    refactor: add to data access module

--- print_2ndo_archivo.py
+++
@@ -1,7 +0,0 @@
-def print_2ndo_archivo(elementos):
-    """
-    funcion que recibe los elementos a imprimir en forma de diccionario
-    Imprime los elementos en un nuevo archivo de texto
-    """
-
-    
