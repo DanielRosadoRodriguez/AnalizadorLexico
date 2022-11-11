01b5b9af8da100dbb59d66eddf6eaac5a8484e75
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-10 18:54:20 -0600

    feat: add values that contain information about each element

--- main_2.py
+++ main_2.py
@@ -5,6 +5,8 @@ import comparisons as c
 from print_error import print_error_message
 
 tokens = read_text(path="input.txt")
+cont_textos = 0
+cont_variables = 0
 
 for line_number, line in enumerate(tokens):
     if line[0] in comentario:
@@ -13,11 +15,30 @@ for line_number, line in enumerate(tokens):
         for element_number, element in enumerate(line):
             if c.is_reservada(element=element, reservadas=elementos_lenguaje):
                 print(element)
+                continue
             elif c.is_text(first_char=element[0], last_char=line[-1][-1]):
                 print("[litalfnum]")
+                cont_textos += 1
+                d_type = "TXT"
+                nombre = element
+                cont = cont_textos
+                continue
             elif element.isnumeric():
                 print("[valorn]")
+                d_type = "VAL"
+                nombre = element
+                valor = int(element)
+                continue
             elif c.is_hexadecimal(element):
                 print("[valorn]")
+                d_type = "VAL"
+                nombre = element
+                valor = int(element, 16)
+                continue
             elif c.is_id(element):
+                cont_variables += 1
                 print("[id]")
+                d_type = "IDS"
+                nombre = element
+                cont = cont_variables
+                continue
