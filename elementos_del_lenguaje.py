c4153ad16ac6cc0aa0223167da4760cb0555ff73
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-10 18:42:08 -0600

    feat: add list of all words of the language

--- elementos_del_lenguaje.py
+++ elementos_del_lenguaje.py
@@ -3,3 +3,7 @@ reservadas = ["PROGRAMA", "FINPROG", "IMPRIME", "LEE"]
 operadores = ["+", "-", "*", "/"]
 asignacion = ["="]
 comentario = ["#"]
+elementos_lenguaje = []
+elementos_lenguaje.extend(reservadas)
+elementos_lenguaje.extend(operadores)
+elementos_lenguaje.extend(asignacion)
