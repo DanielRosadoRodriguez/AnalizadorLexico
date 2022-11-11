630e09790a157700471f3ccd1995c7eca273d775
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-10 21:55:51 -0600

    fix: statement now prints the number of the line where the error happens. error was: statement prints the entire line of the error

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ for line_number, line in enumerate(tokens):
                 lista_texto_1.append("[id]")
             else:
                 there_is_an_error = True
-                print(f"Danger! Error en la linea {line}")
+                print(f"Danger! Error en la linea {line_number}")
                 break
             if not there_is_an_error:
                 element_info = to_dictionary(d_type=d_type, name=nombre, value=valor)
