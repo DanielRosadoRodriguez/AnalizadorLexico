74379af8ea536d4475c7942efb0ce7ec1ad47454
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-10 21:40:25 -0600

    feat: add function that parses the dictionaries and function that formats each line of the new list

--- process_data.py
+++ process_data.py
@@ -8,3 +8,37 @@ def to_dictionary(d_type, name, value):
         }
     }
     return current_dict
+
+
+def format_elements(dicto):
+    """function that receives a dictionary and formats its elements"""
+    formatted_list = f"{dicto['elements']['name']} {dicto['elements']['value']}"
+    return formatted_list
+
+
+def parse_dictionary(dictionaries):
+    """
+    function that receives a list of dictionaries
+    converts them into a processable form
+    returns a list of the elements
+    """
+    texts = []
+    ids = []
+    numeric = []
+    for dictionary in dictionaries:
+        if dictionary["d_type"] == 'VAL':
+            chosen_list = numeric
+        elif dictionary["d_type"] == 'TXT':
+            chosen_list = texts
+        elif dictionary["d_type"] == 'IDS':
+            chosen_list = ids
+        chosen_list.append(format_elements(dictionary))
+
+    final_list = []
+    final_list.append("IDS")
+    final_list.extend(ids)
+    final_list.append("\nTXT")
+    final_list.extend(texts)
+    final_list.append("\nVAL")
+    final_list.extend(numeric)
+    return final_list
